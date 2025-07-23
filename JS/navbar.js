b37f39a8df10d9dbfde7ca3ce33032317792ab86
Omans16 <muhamadnurrohman007@gmail.com>
2025-07-23 09:39:21 +0700

    membuat folder JS Menambahkan file home dan navbar

---
+++ JS/navbar.js
@@ -0,0 +1,14 @@
+  
+  
+  
+  
+  //Navbar
+  const menuToggle = document.getElementById('menu-toggle');
+  const navMenu = document.getElementById('nav-menu');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+
+
+
